4b3dfca0e3b9dff5a2d4dac7e0b4cbc407aca3c6
SalavessaMota <nunosalavessa@hotmail.com>
2024-09-26 15:32:31 +0100

    Add project files.

---
+++ MauiDemo/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage =  new NavigationPage(new MinhaPagina());
+            navPage.BarBackground = Colors.Yellow;
+            navPage.BarTextColor = Colors.Blue;
+
+            MainPage = new TabbedPageDemo();
+        }
+    }
+}
